bf379a73d531e89d2284a23150540a2b094c6761
Pedro Henrique Ferreira da Silva <pedrohferreira005@gmail.com>
2025-12-06 03:06:22 -0300

    update para deploy

--- agentos.py
+++ agentos.py
@@ -1,16 +1,21 @@
+import os
 from agno.os import AgentOS
 from app.agents.agente_produtos import agente_especialista
-
 from fastapi.responses import HTMLResponse
 from fastapi import Request
 from urllib.parse import quote
 
 agent = agente_especialista
 
-from agno.os import AgentOS
-
 agent_os = AgentOS(agents=[agent])
 app = agent_os.get_app()
 
 if __name__ == "__main__":
-    agent_os.serve("agentos:app", reload=True)
+    port = int(os.environ.get("PORT", 7777))
+    
+    agent_os.serve(
+        "agentos:app", 
+        host="0.0.0.0", 
+        port=port, 
+        reload=True
+    )
